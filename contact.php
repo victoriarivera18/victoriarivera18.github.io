60bf1e8d2311441d337209bedc3188dc0cecce42
Victoria Rivera Casanova <46541846+victoriarivera18@users.noreply.github.com>
2020-08-13 17:04:36 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,19 +1,26 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['textarea'];
+    # check if submit button pressed
+    if(isset($_POST['submit'])) {
+        # get all data from contact form
+        $name = $_POST['name'];
+        $visitor_email = $_POST['email'];
+        $message = $_POST['textarea'];
+        
+        
+        $to = "victoriaeugenia2018@gmail.com";
+        $headers = "New Inquiry from Personal website: ".$name;
+        
+        $email_subject = "New Inquiry from Personal website";
+        $email_body = "User Name: ".$name."\n".
+                        "User Email: ".$visitor_email."\n".
+                            "User Message: " .$message."\n";
+        
+        mail($to, $email_subject, $email_body, $headers);
+        header("Location: contact.html?mailsend");
+    }
 
 
     if (($name=="")||($visitor_email=="")||($message=="")){
         echo "All fields are required, please fill <a href=\"\">the form</a> again.";  
-    }else{
-        $email_subject = "New Inquiry from Personal website";
-        $email_body = "User Name: $name.\n".
-                        "User Email: $visitor_email.\n".
-                            "User Message: $message.\n";
-
-        $to = "victoriaeugenia2018@gmail.com";
-        mail($to, $email_subject, $email_body);
-        
     }
 ?>
