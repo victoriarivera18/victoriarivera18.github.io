ac1ec9d928846e34095d8ffcce6437e92e3fbd0d
Victoria Rivera Casanova <46541846+victoriarivera18@users.noreply.github.com>
2020-08-13 16:47:56 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,12 +9,11 @@
     }else{
         $email_subject = "New Inquiry from Personal website";
         $email_body = "User Name: $name.\n".
-                        "User Email: $email.\n".
+                        "User Email: $visitor_email.\n".
                             "User Message: $message.\n";
 
         $to = "victoriaeugenia2018@gmail.com";
         mail($to, $email_subject, $email_body);
-        header('Location: contact-form-thankyou.html');
         
     }
 ?>
